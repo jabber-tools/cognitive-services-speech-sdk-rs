11373ac2bcc526f511f12dc753766c4a2c1a0056
Adam Bezecny <adam.bezecny@gmail.com>
2021-03-01 22:23:50 +0100

    speech config added to main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 use cognitive_services_speech_sdk_rs::audio::{
     input_stream::AudioInputStream, stream_format::AudioStreamFormat,
 };
-use cognitive_services_speech_sdk_rs::config::AudioConfig;
+use cognitive_services_speech_sdk_rs::config::{AudioConfig, SpeechConfig};
 use log::*;
 use std::env;
 
 fn main() {
+    env::set_var("MSSubscriptionKey", "123456789"); // just for now
+    env::set_var("MSServiceRegion", "westeurope");
+
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
-    info!("calling get_wave_format_pcm");
+    info!("calling AudioStreamFormat::get_wave_format_pcm");
     let wave_format = AudioStreamFormat::get_wave_format_pcm(16000, None, None).unwrap();
-    info!("called get_wave_format_pcm {:?}", wave_format);
+    info!(
+        "called AudioStreamFormat::get_wave_format_pcm {:?}",
+        wave_format
+    );
 
-    info!("calling create_push_stream_from_format");
+    info!("calling AudioInputStream::create_push_stream_from_format");
     let push_stream = AudioInputStream::create_push_stream_from_format(wave_format).unwrap();
-    info!("called create_push_stream_from_format {:?}", push_stream);
+    info!(
+        "called AudioInputStream::create_push_stream_from_format {:?}",
+        push_stream
+    );
 
-    info!("calling from_stream_input");
+    info!("calling AudioConfig::from_stream_input");
     let audio_config = AudioConfig::from_stream_input(push_stream).unwrap();
-    info!("called from_stream_input {:?}", audio_config);
+    info!("called AudioConfig::from_stream_input {:?}", audio_config);
+
+    info!("calling SpeechConfig::from_subscription");
+    let speech_config = SpeechConfig::from_subscription(
+        env::var("MSSubscriptionKey").unwrap(),
+        env::var("MSServiceRegion").unwrap(),
+    )
+    .unwrap();
+    info!("called SpeechConfig::from_subscription {:?}", speech_config);
 }
