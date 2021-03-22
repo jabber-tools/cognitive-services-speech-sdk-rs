54c2699103c79947cb86edf3a4a216a6ebbeaac1
Adam Bezecny <adam.bezecny@gmail.com>
2021-03-22 21:41:10 +0100

    audio layout refactored

---
+++ src/audio/audio_input_stream.rs
@@ -0,0 +1,32 @@
+use crate::audio::AudioStreamFormat;
+use crate::error::{convert_err, Result};
+use crate::ffi::{
+    audio_stream_create_push_audio_input_stream, audio_stream_release, SmartHandle,
+    SPXAUDIOSTREAMHANDLE, SPXHANDLE_EMPTY,
+};
+use log::*;
+
+#[derive(Debug)]
+pub struct AudioInputStream {
+    pub handle: SmartHandle<SPXAUDIOSTREAMHANDLE>,
+    // we need to keep format handle in this struct
+    // otherwise it would be dropped after call of
+    // create_push_stream_from_format!
+    format: AudioStreamFormat,
+}
+
+impl AudioInputStream {
+    pub fn create_push_stream_from_format(format: AudioStreamFormat) -> Result<AudioInputStream> {
+        let mut handle = SPXHANDLE_EMPTY;
+        unsafe {
+            let ret = audio_stream_create_push_audio_input_stream(&mut handle, format.handle.get());
+            convert_err(ret, "create_push_stream_from_format error")?;
+            info!("create_push_stream_from_format ok");
+            let result = AudioInputStream {
+                handle: SmartHandle::create("AudioInputStream", handle, audio_stream_release),
+                format,
+            };
+            Ok(result)
+        }
+    }
+}
---
+++ src/audio/audio_stream_format.rs
@@ -0,0 +1,39 @@
+use crate::error::{convert_err, Result};
+use crate::ffi::{
+    audio_stream_format_create_from_waveformat_pcm, audio_stream_format_release, SmartHandle,
+    SPXAUDIOSTREAMFORMATHANDLE, SPXHANDLE_EMPTY,
+};
+use log::*;
+
+#[derive(Debug)]
+pub struct AudioStreamFormat {
+    pub handle: SmartHandle<SPXAUDIOSTREAMFORMATHANDLE>,
+}
+
+impl AudioStreamFormat {
+    pub fn get_wave_format_pcm(
+        samples_per_second: u32,
+        bits_per_sample: Option<u8>,
+        channels: Option<u8>,
+    ) -> Result<AudioStreamFormat> {
+        let mut handle = SPXHANDLE_EMPTY;
+        unsafe {
+            let ret = audio_stream_format_create_from_waveformat_pcm(
+                &mut handle,
+                samples_per_second,
+                bits_per_sample.unwrap_or(16),
+                channels.unwrap_or(1),
+            );
+            convert_err(ret, "get_wave_format_pcm error")?;
+            info!("get_wave_format_pcm ok");
+            let result = AudioStreamFormat {
+                handle: SmartHandle::create(
+                    "AudioStreamFormat",
+                    handle,
+                    audio_stream_format_release,
+                ),
+            };
+            Ok(result)
+        }
+    }
+}
