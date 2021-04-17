c13140fd6a1bd669236ccbcd5f9e9af1d62afadf
Adam Bezecny <adam.bezecny@gmail.com>
2021-04-17 23:17:31 +0200

    integration tests added

---
+++ tests/integration_test.rs
@@ -0,0 +1,96 @@
+use cognitive_services_speech_sdk_rs as msspeech;
+use log::*;
+use std::env;
+
+#[tokio::test]
+async fn speech_to_text() {
+    let filename = env::var("WAVFILENAME").unwrap();
+    let audio_config = msspeech::audio::AudioConfig::from_wav_file_input(&filename).unwrap();
+
+    // before running this text export the below listed variables. Example:
+    // export MSSubscriptionKey=32...
+    // export MSServiceRegion=westeurope
+    // export WAVFILENAME=/tmp/hello_rust.wav
+    // cargo test
+    let speech_config = msspeech::speech::SpeechConfig::from_subscription(
+        env::var("MSSubscriptionKey").unwrap(),
+        env::var("MSServiceRegion").unwrap(),
+    )
+    .unwrap();
+    let mut speech_recognizer =
+        msspeech::speech::SpeechRecognizer::from_config(speech_config, audio_config).unwrap();
+
+    speech_recognizer
+        .set_session_started_cb(|event| info!("set_session_started_cb {:?}", event))
+        .unwrap();
+
+    speech_recognizer
+        .set_session_stopped_cb(|event| info!("set_session_stopped_cb {:?}", event))
+        .unwrap();
+
+    speech_recognizer
+        .set_speech_start_detected_cb(|event| info!("set_speech_start_detected_cb {:?}", event))
+        .unwrap();
+
+    speech_recognizer
+        .set_speech_end_detected_cb(|event| info!("set_speech_end_detected_cb {:?}", event))
+        .unwrap();
+
+    speech_recognizer
+        .set_recognizing_cb(|event| info!("set_recognizing_cb {:?}", event.result.text))
+        .unwrap();
+
+    speech_recognizer
+        .set_recognized_cb(|event| info!("set_recognized_cb {:?}", event))
+        .unwrap();
+
+    speech_recognizer
+        .set_canceled_cb(|event| info!("set_canceled_cb {:?}", event))
+        .unwrap();
+
+    let result = speech_recognizer.recognize_once_async().await.unwrap();
+    info!("got recognition {:?}", result);
+    // actual result might differ as the speech api evolves :)
+    // assert_eq!(result.text, "Hello rust.");
+}
+
+#[tokio::test]
+async fn text_to_speech() {
+    let pull_stream = msspeech::audio::PullAudioOutputStream::create_pull_stream().unwrap();
+    let audio_config = msspeech::audio::AudioConfig::from_stream_output(&pull_stream).unwrap();
+
+    // before running this text export the below listed variables. Example:
+    // export MSSubscriptionKey=32...
+    // export MSServiceRegion=westeurope
+    // cargo test
+    let speech_config = msspeech::speech::SpeechConfig::from_subscription(
+        env::var("MSSubscriptionKey").unwrap(),
+        env::var("MSServiceRegion").unwrap(),
+    )
+    .unwrap();
+    let mut speech_synthesizer =
+        msspeech::speech::SpeechSynthesizer::from_config(speech_config, audio_config).unwrap();
+
+    speech_synthesizer
+        .set_synthesizer_started_cb(|event| info!("synthesizer_started_cb {:?}", event))
+        .unwrap();
+
+    speech_synthesizer
+        .set_synthesizer_synthesizing_cb(|event| info!("synthesizer_synthesizing_cb {:?}", event))
+        .unwrap();
+
+    speech_synthesizer
+        .set_synthesizer_completed_cb(|event| info!("synthesizer_completed_cb {:?}", event))
+        .unwrap();
+
+    speech_synthesizer
+        .set_synthesizer_canceled_cb(|event| info!("synthesizer_canceled_cb {:?}", event))
+        .unwrap();
+
+    match speech_synthesizer.speak_text_async("Hello Rust!").await {
+        Err(err) => error!("speak_text_async error {:?}", err),
+        Ok(speech_audio_bytes) => {
+            info!("speech_audio_bytes {:?}", speech_audio_bytes);
+        }
+    }
+}
