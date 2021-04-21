161ea0b6763e694fa41dca8a99bf6bf05a3a2f54
Adam Bezecny <adam.bezecny@gmail.com>
2021-04-21 20:31:46 +0200

    CancellationReason extended

--- src/common/cancellation_reason.rs
+++ src/common/cancellation_reason.rs
@@ -5,13 +5,16 @@ pub enum CancellationReason {
     Error = 1,
     /// Indicates that the end of the audio stream was reached.
     EndOfStream = 2,
+    /// Indicates that request was cancelled by the user.
+    CancelledByUser = 3,
 }
 
 impl CancellationReason {
     pub fn from_u32(code: u32) -> Self {
         return match code {
             1 => CancellationReason::Error,
-            _ => CancellationReason::EndOfStream,
+            2 => CancellationReason::EndOfStream,
+            _ => CancellationReason::CancelledByUser,
         };
     }
 }
