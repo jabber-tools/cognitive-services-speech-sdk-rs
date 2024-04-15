be05bedf262513590caa0bf4199100499474919a
Fu Wang <5633310+wangfu91@users.noreply.github.com>
2024-04-15 16:14:03 +0800

    Upgrade speech sdk to version 1.37.0 (#12)
    * Update speech sdk version for linux.
    
    * Update speech sdk for macOS.
    
    * Update build.rs for linux.
    
    * Bring back some removed comments.
    
    * Canonicalize the parent_dir to make the build script more robust.

--- build.rs
+++ build.rs
@@ -7,20 +7,20 @@ use std::{
     str,
 };
 
-#[cfg(not(target_os = "macos"))]
-const LINUX_SDK_URL: &str  = "https://github.com/jabber-tools/cognitive-services-speech-sdk-rs-files/blob/main/SpeechSDK/1.22.0/linux/SpeechSDK-Linux-1.22.0.tar.gz?raw=true";
+const SPEECH_SDK_VERSION: &str = "1.37.0";
 
-#[cfg(not(target_os = "macos"))]
 fn download_file(url: &str, dst: &str) {
     Command::new("curl")
-        .args(&["-SL", url, "-o", dst])
+        .args(["-SL", url, "-o", dst])
         .status()
         .expect("failed to download Speech SDK!");
 }
 
-#[cfg(not(target_os = "macos"))]
+#[cfg(target_os = "linux")]
 fn main() {
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let linux_sdk_url = format!(
+        "https://csspeechstorage.blob.core.windows.net/drop/{SPEECH_SDK_VERSION}/SpeechSDK-Linux-{SPEECH_SDK_VERSION}.tar.gz");
+
     // copying SpeechSDK from local folder just worked fine but crates.io allows
     // only 10 MB to be uploaded per crate. Because of that SpeechSDK shared libraries
     // were moved to separate repository (https://github.com/jabber-tools/cognitive-services-speech-sdk-rs-files)
@@ -41,44 +41,57 @@ fn main() {
         Path::new(&out_path).join("libMicrosoft.CognitiveServices.Speech.extension.codec.so"),
     )
     .unwrap();
-
     println!("cargo:rustc-link-search=native={}", out_path.display());
     println!("cargo:rustc-link-lib=dylib=Microsoft.CognitiveServices.Speech.core");
     */
 
+    let parent_dir = PathBuf::from("./SpeechSDK").join("linux");
+    if !parent_dir.exists() {
+        fs::create_dir_all(&parent_dir).unwrap();
+    }
+    let parent_dir = fs::canonicalize(parent_dir).unwrap();
+
     let mut renew = env::var("RENEW_SDK").map(|v| v == "1").unwrap_or(false);
-    let sdk_path = out_path.join("SpeechSDK").join("linux");
-    if !sdk_path.exists() {
+    let sdk_output_dir = parent_dir.join("sdk_output");
+    if !sdk_output_dir.exists() || fs::read_dir(&sdk_output_dir).unwrap().next().is_none() {
         renew = true;
-        fs::create_dir_all(&sdk_path).unwrap();
+        fs::create_dir_all(&sdk_output_dir).unwrap();
+    }
+
+    let sdk_tar_file = parent_dir.join(format!("SpeechSDK-Linux-{SPEECH_SDK_VERSION}.tar.gz"));
+    if !sdk_tar_file.exists() {
+        download_file(linux_sdk_url.as_str(), sdk_tar_file.to_str().unwrap());
     }
 
     if renew {
-        let dw_file = out_path.join("linux.sdk");
-        let sdk_file = dw_file.to_str().unwrap();
-        download_file(LINUX_SDK_URL, sdk_file);
         let args = [
             "--strip",
             "1",
             "-xzf",
-            sdk_file,
+            sdk_tar_file.to_str().unwrap(),
             "-C",
-            sdk_path.to_str().unwrap(),
+            sdk_output_dir.to_str().unwrap(),
         ];
-        Command::new("tar").args(&args).status().unwrap();
+        Command::new("tar").args(args).status().unwrap();
     }
 
     #[cfg(target_arch = "x86")]
-    let lib_path = sdk_path.join("lib").join("x86");
+    let lib_path = sdk_output_dir.join("lib").join("x86");
     #[cfg(target_arch = "x86_64")]
-    let lib_path = sdk_path.join("lib").join("x64");
+    let lib_path = sdk_output_dir.join("lib").join("x64");
     #[cfg(target_arch = "arm")]
-    let lib_path = sdk_path.join("lib").join("arm32");
+    let lib_path = sdk_output_dir.join("lib").join("arm32");
     #[cfg(target_arch = "aarch64")]
-    let lib_path = sdk_path.join("lib").join("arm64");
+    let lib_path = sdk_output_dir.join("lib").join("arm64");
 
     let mut inc_arg = String::from("-I");
-    inc_arg.push_str(sdk_path.join("include").join("c_api").to_str().unwrap());
+    inc_arg.push_str(
+        sdk_output_dir
+            .join("include")
+            .join("c_api")
+            .to_str()
+            .unwrap(),
+    );
 
     println!("cargo:rustc-link-search=native={}", lib_path.display());
     println!("cargo:rustc-link-lib=dylib=Microsoft.CognitiveServices.Speech.core");
@@ -95,12 +108,8 @@ fn main() {
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings_builder = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
+        // The input header we would like to generate bindings for.
         .header("c_api/wrapper.h")
-        // use line below when building from local SpeechSDK folder
-        //.clang_arg("-ISpeechSDK/include/c_api/")
-        // use this line when building from downloaded files in OUT_DIR
         .clang_arg(inc_arg.as_str());
 
     #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
@@ -120,7 +129,7 @@ fn main() {
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    // Write the bindings to the src/ffi/bindings.rs file.
     bindings
         .write_to_file("src/ffi/bindings.rs")
         .expect("Couldn't write bindings!");
@@ -132,14 +141,42 @@ fn main() {
     all(target_os = "macos", target_arch = "x86_64")
 ))]
 fn main() {
-    let speek_sdk_root = env::var("MACOS_SPEECHSDK_ROOT").expect(
-        "Set environment variable MACOS_SPEECHSDK_ROOT with location of MS Speech SDK library.",
-    );
+    let mac_sdk_url = format!("https://csspeechstorage.blob.core.windows.net/drop/{SPEECH_SDK_VERSION}/MicrosoftCognitiveServicesSpeech-MacOSXCFramework-{SPEECH_SDK_VERSION}.zip");
 
-    println!("cargo:rustc-link-search=framework={}/MicrosoftCognitiveServicesSpeech.xcframework/macos-arm64_x86_64", speek_sdk_root);
+    let parent_dir = PathBuf::from("./SpeechSDK").join("macOS");
+    if !parent_dir.exists() {
+        fs::create_dir_all(&parent_dir).unwrap();
+    }
+    let parent_dir = fs::canonicalize(parent_dir).unwrap();
+
+    let mut renew = env::var("RENEW_SDK").map(|v| v == "1").unwrap_or(false);
+    let sdk_output_dir = parent_dir.join("sdk_output");
+    if !sdk_output_dir.exists() || fs::read_dir(&sdk_output_dir).unwrap().next().is_none() {
+        renew = true;
+        fs::create_dir_all(&sdk_output_dir).unwrap();
+    }
+
+    let sdk_zip_file = parent_dir.join(format!(
+        "MicrosoftCognitiveServicesSpeech-MacOSXCFramework-{SPEECH_SDK_VERSION}.zip"
+    ));
+    if !sdk_zip_file.exists() {
+        download_file(mac_sdk_url.as_str(), sdk_zip_file.to_str().unwrap());
+    }
+
+    if renew {
+        let args = [
+            "-o",                           // Overwrite files without prompting
+            sdk_zip_file.to_str().unwrap(), // The zip file
+            "-d",
+            sdk_output_dir.to_str().unwrap(), // The directory to extract to
+        ];
+        Command::new("unzip").args(args).status().unwrap();
+    }
+
+    println!("cargo:rustc-link-search=framework={}/MicrosoftCognitiveServicesSpeech.xcframework/macos-arm64_x86_64", sdk_output_dir.display());
     println!("cargo:rustc-link-lib=framework=MicrosoftCognitiveServicesSpeech");
 
-    let inc_arg = format!("-I{}/MicrosoftCognitiveServicesSpeech.xcframework/macos-arm64_x86_64/MicrosoftCognitiveServicesSpeech.framework/Headers", speek_sdk_root);
+    let inc_arg = format!("-I{}/MicrosoftCognitiveServicesSpeech.xcframework/macos-arm64_x86_64/MicrosoftCognitiveServicesSpeech.framework/Headers", sdk_output_dir.display());
 
     let bindings_builder = bindgen::Builder::default()
         .header("c_api/wrapper.h")
